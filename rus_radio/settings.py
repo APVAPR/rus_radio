24fb994d30f0e77bf2e6229f5cdf6ee93a051cd2
APVAPR <sasha.pozniak@gmail.com>
2022-02-10 20:35:53 +0300

    staticfiles

--- rus_radio/settings.py
+++ rus_radio/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 
 import dj_database_url
+import django_heroku
 from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'rus_radio.urls'
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'rus_radio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,8 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [(
+    os.path.join(BASE_DIR, 'static')
+)]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -125,3 +130,4 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
--- rus_radio/urls.py
+++ rus_radio/urls.py
@@ -13,11 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.conf.urls import url
+from django.views.static import serve as mediaserve
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rus_radio import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('payroll.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# else:
+#     urlpatterns += [
+#         url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+#             mediaserve, {'document_root': settings.MEDIA_ROOT}),
+#         url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+#             mediaserve, {'document_root': settings.STATIC_ROOT}),
+#     ]
